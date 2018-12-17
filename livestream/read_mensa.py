d744bf3235cdc83c342226d5b4a6721e01714cce
Tim <30591400+quereinsteiger@users.noreply.github.com>
2018-12-17 10:38:02 +0100

    Update read_mensa.py
    print the menu from today to the day after tomorrow for each url in urls.
    
    minimize redundancy

--- livestream/read_mensa.py
+++ livestream/read_mensa.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import pandas as pd
@@ -5,12 +6,15 @@ import pandas as pd
 urls = {"mensa" : "https://www.stw-thueringen.de/deutsch/mensen/einrichtungen/ilmenau/mensa-ehrenberg.html",
         "nano" : "https://www.stw-thueringen.de/deutsch/mensen/einrichtungen/ilmenau/cafeteria-nanoteria.html"}
 data = {}
+days = {0: 'heute', 1: 'morgen', 2 : 'übermorgen'}
 
 for index, url in urls.items():
     data[index] = pd.read_html(url)    
 
-print("\n\nIn der Mensa gibt es heute: \n")
-print(data['mensa'][0][0:].to_string())
+for key in urls.keys():
+    for index, day in days.items():
+        print("\n\nIn der {mensa} gibt es {day}: \n".format( mensa = key, 
+                                                             day = day))
+        print(data[key][index][0:].to_string())
 
-print("\n\nIn der Nano gibt es heute: \n")
-print(data['nano'][0][0:].to_string())
+input()
