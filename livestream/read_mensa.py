7c6cb43d3c5d2ebaeaa8396b030f56603aeff5d4
Tim <30591400+quereinsteiger@users.noreply.github.com>
2018-12-18 09:31:06 +0100

    Update read_mensa.py
    improve readability

--- livestream/read_mensa.py
+++ livestream/read_mensa.py
@@ -3,18 +3,18 @@
 
 import pandas as pd
 
-urls = {"mensa" : "https://www.stw-thueringen.de/deutsch/mensen/einrichtungen/ilmenau/mensa-ehrenberg.html",
-        "nano" : "https://www.stw-thueringen.de/deutsch/mensen/einrichtungen/ilmenau/cafeteria-nanoteria.html"}
+mensen = {"mensa" : "https://www.stw-thueringen.de/deutsch/mensen/einrichtungen/ilmenau/mensa-ehrenberg.html",
+          "nano" : "https://www.stw-thueringen.de/deutsch/mensen/einrichtungen/ilmenau/cafeteria-nanoteria.html"}
 data = {}
 days = {0: 'heute', 1: 'morgen', 2 : 'übermorgen'}
 
-for index, url in urls.items():
-    data[index] = pd.read_html(url)    
+for mensa, url in mensen.items():
+    data[mensa] = pd.read_html(url)    
 
-for key in urls.keys():
+for mensa in data:
     for index, day in days.items():
-        print("\n\nIn der {mensa} gibt es {day}: \n".format( mensa = key, 
+        print("\n\nIn der {mensa} gibt es {day}: \n".format( mensa = mensa, 
                                                              day = day))
-        print(data[key][index][0:].to_string())
+        print(data[mensa][index][0:].to_string())
 
 input()
